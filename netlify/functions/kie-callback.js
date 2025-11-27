7c9205cbe19b4706cd66a2fb223b1986fb9ff126
Hansora-ai <hansora.ai@mail.ru>
2025-11-27 23:18:23 +0400

    Update kie-callback.js

--- netlify/functions/kie-callback.js
+++ netlify/functions/kie-callback.js
@@ -182,7 +182,6 @@ function isAllowedFinal(u){
   if (!isUrl(u)) return false;
   const h = host(u);
   if (!ALLOWED_HOSTS.has(h)) return false;
-  if (!/\/(workers|f|m|r|h)\//i.test(u)) return false;
   return true;
 }
 
