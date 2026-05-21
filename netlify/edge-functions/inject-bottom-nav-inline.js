535723e9d4a408ee461b5aa10c4664b29e1ec012
Hansora-ai <hansora.ai@mail.ru>
2026-05-22 00:42:32 +0400

    Update inject-bottom-nav-inline.js

--- netlify/edge-functions/inject-bottom-nav-inline.js
+++ netlify/edge-functions/inject-bottom-nav-inline.js
@@ -327,7 +327,7 @@ export default async (request, context) => {
       <span class="hs-radial-icon"><svg viewBox="0 0 24 24"><path d="M4 5h16v14H4z"></path><path d="M8 13l2.5-3 3 4 2-2.5L20 17"></path><circle cx="8" cy="8" r="1.5"></circle></svg></span>
       <b>Image</b>
     </a>
-    <a class="hs-radial-item hs-radial-video" href="/search-models.html?type=video">
+    <a class="hs-radial-item hs-radial-video" href="/search-models.html?model=kling-3">
       <span class="hs-radial-icon"><svg viewBox="0 0 24 24"><path d="M4 6h11v12H4z"></path><path d="M15 10l5-3v10l-5-3"></path></svg></span>
       <b>Video</b>
     </a>
@@ -360,7 +360,7 @@ export default async (request, context) => {
     </header>
     <div class="links">
       <a href="/search-models.html"><svg viewBox="0 0 24 24"><path d="M4 5h16v14H4z"></path><path d="M8 13l2.5-3 3 4 2-2.5L20 17"></path></svg> Image</a>
-      <a href="/search-models.html?type=video"><svg viewBox="0 0 24 24"><path d="M4 6h11v12H4z"></path><path d="M15 10l5-3v10l-5-3"></path></svg> Video</a>
+      <a href="/search-models.html?model=kling-3"><svg viewBox="0 0 24 24"><path d="M4 6h11v12H4z"></path><path d="M15 10l5-3v10l-5-3"></path></svg> Video</a>
       <a href="/audio.html"><svg viewBox="0 0 24 24"><path d="M5 10v4"></path><path d="M9 7v10"></path><path d="M13 5v14"></path><path d="M17 8v8"></path></svg> Audio</a>
       <button class="hs-feature-toggle" id="hs-feature-toggle" type="button">
         <span><svg viewBox="0 0 24 24"><path d="M12 3l1.8 5.2L19 10l-5.2 1.8L12 17l-1.8-5.2L5 10l5.2-1.8L12 3z"></path></svg> Features</span>
